da4a74fd201cb0a620fdaca4929374b8422d92a9
Mark Honeychurch <mark@honeychurch.org>
2022-07-10 22:56:57 +1200

    Working code

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const {createApp} = Vue;
+// const {createApp} = Vue;
 // import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 // import {BootstrapVue} from 'https://unpkg.com/bootstrap-vue/dist/bootstrap-vue.esm.js';
 // import vueChart3 from 'https://unpkg.com/vue-chart-3/dist/index.js';
 
-createApp({
+var vm = new Vue({
+// createApp({
+	el: '#app',
 	data() {
 		return {
-			dataset: null,
+			names: null,
 			start: 1900,
-			end: new Date().getFullYear(),
-			names: ['Mark', 'Karen'],
+			end: 2021,
+			year: 2021,
+			top: 10,
+			compare: ['Mark', 'Karen'],
 		};
 	},
 	computed: {
 		years() {
 			return [...Array(this.end - this.start).keys()].map((e) => e + this.start);
 		},
+		topx() {
+			return Object.keys(this.names).map((name) => ({name, number: this.names[name][this.year] || 0})).sort((a, b) => b.number - a.number).slice(0, this.top);
+		},
 		data() {
-			if (!this.dataset) return {};
+			if (!this.names) return {};
 			return {
 				labels: this.years,
 				xaxis: {
 					categories: this.years,
 				},
-				datasets: this.names.map((name) => ({
+				datasets: this.compare.map((name) => ({
 					name,
-					data: this.dataset[name] ? this.years.map((year) => this.dataset[name][year] || 0) : [],
+					data: this.names[name] ? this.years.map((year) => this.names[name][year] || 0) : [],
 				})),
 			};
 		},
@@ -34,9 +41,9 @@ createApp({
 		fetch('/names.json', {
 			headers: {'Content-type': 'application/json'},
 		}).then((res) => res.json()).then((response) => {
-			this.dataset = response;
+			this.names = response;
 		}).catch( (error) => {
-			this.dataset = {};
+			this.names = {};
 		});
 	},
-}).mount('#app');
+}); // .mount('#app');
--- process.js
+++ process.js
@@ -1,7 +1,7 @@
 import {readFileSync, writeFileSync} from 'fs';
 import {parse} from 'csv-parse/sync';
 
-const records = parse(readFileSync('baby-names-2021-01-07.csv'), {columns: true, skip_empty_lines: true});
+const records = parse(readFileSync('baby-names-2022-01-07.csv'), {columns: true, skip_empty_lines: true});
 
 const names = records.reduce((names, record) => {
 	if (!names[record.Name]) {
@@ -12,3 +12,17 @@ const names = records.reduce((names, record) => {
 }, {});
 
 writeFileSync('names.json', JSON.stringify(names));
+
+const years = [...Array(2021 - 1900).keys()].map((index) => index + 1900);
+const yearnames = records.reduce((names, record) => {
+	if (!names[record.Name]) {
+		names[record.Name] = {};
+	}
+	names[record.Name][parseInt(record.Year)] = parseInt(record.Count);
+	return names;
+}, {});
+for (const name of Object.keys(yearnames)) {
+	yearnames[name] = years.map((year) => yearnames[name][year] || 0);
+}
+
+writeFileSync('yearnames.json', JSON.stringify(yearnames));
