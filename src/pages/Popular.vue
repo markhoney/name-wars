6992d84e47cce4ca024d06a42534e7ffd750ed52
Mark Honeychurch <mark@honeychurch.org>
2022-07-21 09:54:53 +1200

    Adding popular page

--- src/pages/Popular.vue
+++ src/pages/Popular.vue
@@ -1,7 +1,7 @@
 <template>
 	<b-col cols="12" class="mb-5">
-		<h2>New Names</h2>
-		<p>Below is a graph of how many new names were seen in the list of baby names for the first time in a given year and a list of new names that were first used in each year.</p>
+		<h2>Polular Names</h2>
+		<p>Below is a graph showing how many times the most popular name was used in each year.</p>
 	</b-col>
 	<b-col cols="12">
 		<b-card>
@@ -21,36 +21,36 @@
 	<b-col cols="12">
 		<vue-apex-charts type="bar" :series="series" :options="chartOptions" />
 	</b-col>
-	<b-col sm="6" lg="4">
-		<new v-model="year" />
-	</b-col>
-	<b-col cols="12">
-		<vue-apex-charts type="bar" :series="secondSeries" :options="secondChartOptions" />
-	</b-col>
 </template>
 
 <script>
-	import New from '../components/New.vue';
 	import Years from '../components/Years.vue';
 	import VueApexCharts from "vue3-apexcharts";
 	export default {
-		components: {New, Years, VueApexCharts},
+		components: {Years, VueApexCharts},
 		props: {
 			value: {
 				type: String,
 				default: '',
 			},
 		},
-		data() {
-			return {
-				first: 1901,
-				last: 2021,
-				year: 2021,
-			};
-		},
 		computed: {
 			years() {
 				return this.$names.years(this.first, this.last);
+			},
+			popular() {
+				return this.years.map((year) => {
+					const popular = {year, name: '', number: 0};
+					for (const name of Object.keys(this.$names.names)) {
+						for (const sex of ['M', 'F']) {
+							if ((this.$names.names[name][year]?.[sex] || 0) > popular.number) {
+								popular.name = name;
+								popular.number = this.$names.names[name][year][sex];
+							}
+						}
+					}
+					return popular;
+				});
 			},
 			chartOptions() {
 				return {
@@ -61,33 +61,15 @@
 						title: {text: 'Year'},
 					},
 					yaxis: {
-						title: {text: 'Number of new names'},
+						title: {text: 'Most popular name'},
 					},
 				};
 			},
 			series() {
+				if (!this.popular) return [];
 				return [{
 					name: 'New names',
-					data: this.years.map((year) => this.$names.new[year].length),
-				}];
-			},
-			secondChartOptions() {
-				return {
-					colors: ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'],
-					xaxis: {
-						type: 'numeric',
-						categories: this.years,
-						title: {text: 'Year'},
-					},
-					yaxis: {
-						title: {text: 'Number of names'},
-					},
-				};
-			},
-			secondSeries() {
-				return [{
-					name: 'New names',
-					data: this.years.map((year) => Object.values(this.$names.names).filter((name) => name[year]).length),
+					data: this.years.map((year) => this.popular.find((pop) => year === pop.year).number),
 				}];
 			},
 		},
